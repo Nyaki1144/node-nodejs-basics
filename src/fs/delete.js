ed0d46c30b786ae5c0cf844b3144e8f8e971051d
Nyaki1144 <narek1144@gmail.com>
2022-06-02 17:08:21 +0400

    feat: add  list.js solution

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -2,7 +2,6 @@ import fs from 'fs/promises';
 import path from 'path';
 import { URL } from 'url';
 
-
 export const remove = async () => {
     const  __dirname = decodeURI(new URL('.', import.meta.url).pathname);
     const filesPath = path.join(__dirname, "files" ,'fileToRemove.txt');
--- src/fs/list.js
+++ src/fs/list.js
@@ -1,3 +1,19 @@
+import fs from 'fs/promises';
+import path from 'path';
+import { URL } from 'url';
+
 export const list = async () => {
-    // Write your code here 
+  const  __dirname = decodeURI(new URL('.', import.meta.url).pathname);
+  const filesPath = path.join(__dirname, "files");
+
+  fs.access(filesPath)
+  .then((res, rej) => fs.readdir(filesPath))
+  .then((res, rej) => {
+    res.forEach(element => {
+      console.log(element)
+    });
+  })
+  .catch((res, rej) => {
+    console.error('FS operation failed')
+  })
 };
