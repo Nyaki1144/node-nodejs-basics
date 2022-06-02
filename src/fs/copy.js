1572ded1aa277c44045c1472a632bdd0d6add7b4
Nyaki1144 <narek1144@gmail.com>
2022-06-02 16:11:28 +0400

    feat: add copy.js solution

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,3 +1,28 @@
+import fs from 'fs/promises';
+import path from 'path';
+import { URL } from 'url';
+
+
+
 export const copy = async () => {
-    // Write your code here 
+  const  __dirname = decodeURI(new URL('.', import.meta.url).pathname);
+  const filesPath = path.join(__dirname, 'files');
+  const copyFilesPath = path.join(__dirname, 'files_copy');
+
+  fs.access(filesPath)
+  .then((res, rej) => {})
+  .catch(() => console.error('FS operation failed'))
+
+  fs.mkdir(copyFilesPath)
+  .then(() => fs.readdir(filesPath))
+  .then((res, rej) => {
+    res.forEach(el => {
+      fs.readFile(path.join(filesPath, el))
+      .then((res, rej) => {fs.writeFile(path.join(copyFilesPath, el), res)})
+  });
+  })
+  .catch((res, rej) => console.error('FS operation failed'))
 };
+
+
+copy()
