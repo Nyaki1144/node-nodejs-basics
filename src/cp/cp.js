6befc27704edc83a0c805e852edb649f40d47019
Nyaki1144 <narek1144@gmail.com>
2022-06-06 00:00:44 +0400

    feat: add cp solution

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,3 +1,13 @@
+import { fork } from "child_process";
+import path from 'path';
+import { URL } from 'url';
+
+const  __dirname = decodeURI(new URL('.', import.meta.url).pathname);
+const filesPath = path.join(__dirname, 'files', 'script.js');
+
 export const spawnChildProcess = async (args) => {
-    // Write your code here
+  fork(`${filesPath}`, args);
 };
+
+const argument = process.argv;
+spawnChildProcess(argument);
