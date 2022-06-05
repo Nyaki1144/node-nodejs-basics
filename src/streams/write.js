95ea7bcef611aabafa7c8809789fd0a47709d27f
Nyaki1144 <narek1144@gmail.com>
2022-06-05 10:23:20 +0400

    feat: add write.js solution

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,3 +1,17 @@
+import fs from "fs"
+import path from "path";
+import process from 'process';
+
+
 export const write = async () => {
-    // Write your code here 
+  const  __dirname = decodeURI(new URL('.', import.meta.url).pathname);
+  const filesPath = path.join(__dirname, 'files', 'fileToWrite.txt');
+
+  const writeCreateFiles = fs.createWriteStream(filesPath)
+
+  process.stdin.on('data', (data) =>{
+    writeCreateFiles.write(data)
+  });
 };
+
+write() 
