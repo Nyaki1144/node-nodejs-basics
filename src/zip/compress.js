15943bb630df1fe44576ecb73df632270c73c633
Nyaki1144 <narek1144@gmail.com>
2022-06-05 12:37:54 +0400

    feat: create zip solution

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,3 +1,16 @@
+import fs from 'fs';
+import   zlib   from 'zlib';
+import path from 'path';
+
 export const compress = async () => {
-    // Write your code here 
+    const  __dirname = decodeURI(new URL('.', import.meta.url).pathname);
+    const filesPath = path.join(__dirname, 'files');
+
+    const gzip = zlib.createGzip();
+
+    const input = fs.createReadStream(path.join(filesPath, 'fileToCompress.txt'));
+    const output = fs.createWriteStream(path.join(filesPath, 'fileToCompress.txt.gz'));
+  
+    input.pipe(gzip).pipe(output)
 };
+
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,3 +1,15 @@
+import fs from 'fs';
+import   zlib   from 'zlib';
+import path from 'path';
+
 export const decompress = async () => {
-    // Write your code here 
+    const  __dirname = decodeURI(new URL('.', import.meta.url).pathname);
+    const filesPath = path.join(__dirname, 'files');
+
+    const gzip = zlib.createGunzip();
+
+    const input = fs.createReadStream(path.join(filesPath, 'fileToCompress.txt.gz'));
+    const output = fs.createWriteStream(path.join(filesPath, 'fileToCompress.txt'));
+  
+    input.pipe(gzip).pipe(output)
 };
