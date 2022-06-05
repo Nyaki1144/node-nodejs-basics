e8c27c6f4d6252969e4d3155c4188095595f554d
Nyaki1144 <narek1144@gmail.com>
2022-06-05 11:59:37 +0400

    feat: add transform.js solution

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,3 +1,19 @@
+import process from 'process';
+import { Transform } from 'stream';
+
 export const transform = async () => {
-    // Write your code here 
+    class CounterTransform extends Transform {
+        _transform(chunk, encoding, cb) {
+          try {   
+            let revers = chunk.toString().split('').reverse().join('');
+            cb(null, revers)
+          } 
+          catch (err) {
+            cb(err);
+          }
+        }   
+    }
+
+    const reversChunk = new CounterTransform();
+    process.stdin.pipe(reversChunk).pipe(process.stdout);
 };
